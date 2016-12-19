7d323b8539673596842317ed3e1f8a3d995bd8a6
Samyak Jain <samyakjain3098@gmail.com>
2016-12-19 11:08:12 +0000

    number formatted all output

--- views/portfolio.php
+++ views/portfolio.php
@@ -1,3 +1,28 @@
-<div>
-    <iframe allowfullscreen frameborder="0" height="315" src="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&iv_load_policy=3&rel=0" width="420"></iframe>
-</div>
+<table class="table table-striped">
+    <tr>
+        <td>  <b>Name</b>   </td>
+        <td>  <b>Symbol</b> </td>
+        <td>  <b>Shares</b> </td>
+        <td>  <b>Cost</b>   </td>
+        <td>  <b>Total Price</b>   </td>
+    </tr>
+    <?php extract($values);
+    foreach ($positions as $position): 
+    $total_price = $position["shares"] * $position["price"]; 
+    $total_price = number_format($total_price, 2); 
+    $position["price"] = number_format($position["price"], 2); ?>
+    
+        <tr>
+            <td><?= $position["name"] ?></td>
+            <td><?= $position["symbol"] ?></td>
+            <td><?= $position["shares"] ?></td>
+            <td>$<?= $position["price"] ?></td>
+            <td>$<?= $total_price ?></td>
+        </tr>
+    
+    <?php endforeach ?>
+    <tr>
+        <td><b>CASH BALANCE</b></td>
+        <td colspan="4">$<?= $cash_balance[0]["cash"] ?></td>
+    </tr>
+</table>
